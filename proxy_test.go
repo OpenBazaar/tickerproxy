e52aea7715b7883bd1e3a88bcea15f6632e96632
Tyler Smith <tylersmith.me@gmail.com>
2017-01-05 12:05:16 -0800

    TESTS: Increase test coverage to ~70%.

--- proxy_test.go
+++ proxy_test.go
@@ -1,14 +1,17 @@
 package tickerproxy
 
 import (
+	"io/ioutil"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gocraft/health"
 	"github.com/jarcoal/httpmock"
 )
 
 const testResponseBody = `{"BTC": {"low": 0, "high": 1200.00}}`
 
-func TestProxyFetch(t *testing.T) {
+func TestProxy(t *testing.T) {
 	// Create external http mocks
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -16,6 +19,7 @@ func TestProxyFetch(t *testing.T) {
 
 	// Create a proxy
 	proxy := New(1, "pubkey", "privkey")
+	proxy.SetStream(newTestStream())
 
 	// Fetch data
 	err := proxy.Fetch()
@@ -23,7 +27,31 @@ func TestProxyFetch(t *testing.T) {
 		t.Error(err)
 	}
 
-	if string(proxy.lastResponseBody) != testResponseBody {
+	// Make sure we get the correct response
+	if proxy.String() != testResponseBody {
 		t.Fatal("Incorrect response body.")
 	}
+
+	go proxy.Start()
+
+	// Try the HTTP handler
+	rr := httptest.NewRecorder()
+	proxy.ServeHTTP(rr, httptest.NewRequest("GET", "http://ticker.openbazaar.org/api", nil))
+	result := rr.Result()
+
+	// Check that the response body is correct
+	responseBody, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(responseBody) != testResponseBody {
+		t.Fatal("Incorrect response body.")
+	}
+
+	proxy.Stop()
+}
+
+func newTestStream() *health.Stream {
+	return health.NewStream()
 }
